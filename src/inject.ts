cbdb605b4b3224ef80e0fe8f184dfcaa6fa635f8
a <a@tuxpa.in>
2025-04-01 22:34:51 -0500

    update morpho package so we dont need to manually inject corn

--- src/inject.ts
+++ src/inject.ts
@@ -2,7 +2,7 @@ import { MAINNET_CHAINS } from "@gfxlabs/oku-chains";
 import {addresses, ChainId, ChainUtils, NATIVE_ADDRESS, unwrappedTokensMapping} from "@morpho-org/blue-sdk";
 
 for(const chain of MAINNET_CHAINS) {
-  if(ChainUtils.BLUE_AVAILABLE_CHAINS.includes(chain.id as ChainId)) {
+  if(Object.keys(ChainUtils.CHAIN_METADATA).includes(`${chain.id}`)) {
     // skip since its already in the code
     continue;
   }
@@ -20,16 +20,11 @@ for(const chain of MAINNET_CHAINS) {
   ) {
     continue
   }
-  ChainUtils.BLUE_AVAILABLE_CHAINS.push(chain.id as ChainId);
   ChainUtils.CHAIN_METADATA[chain.id as ChainId] = {
     name: chain.name,
     id: chain.id as ChainId,
     nativeCurrency: chain.nativeCurrency,
-    defaultRpcUrl: chain.rpcUrls.default.http[0],
     explorerUrl: chain.blockExplorers.default.url,
-    isTestnet: false,
-    shortName: chain.name,
-    logoSrc: chain.logoUrl,
     identifier: chain.internalName,
   }
 
